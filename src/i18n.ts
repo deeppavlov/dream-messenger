0df4cad1ddfe576f4626716044ee2c3e5ea64196
Artem M <artemmarin91@mail.ru>
2024-02-01 01:50:32 +0300

    language changed to Russian

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,12 @@
 import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
 i18n
   .use(initReactI18next)
-  .use(LanguageDetector)
   .use(Backend)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'ru',
     debug: false,
     react: {
       transEmptyNodeValue: '',
